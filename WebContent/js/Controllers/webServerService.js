9726ebfaec20133711b40a657e5dc37fb16f4a21
senzitgit <34739394+senzitgit@users.noreply.github.com>
2018-01-17 23:45:32 +0530

    Version 2.4

---
+++ WebContent/js/Controllers/webServerService.js
@@ -0,0 +1,8 @@
+var webServerUrl = "http://192.168.10.50:8080/EvidencerPortal/AppServerService.jsp";
+var captureUrl = "http://192.168.10.50:8080/EvidencerPortal/SaveAttachmentService.jsp";
+var captureUploadUrl = "http://192.168.10.50:8080/EvidencerPortal/CaptureAttachmentService.jsp";
+
+
+/*var webServerUrl = "https://judiciary.mybluemix.net/AppServerService.jsp";
+var captureUrl = "https://judiciary.mybluemix.net/SaveAttachmentService.jsp";
+var captureUploadUrl = "https://judiciary.mybluemix.net/CaptureAttachmentService.jsp"; */
